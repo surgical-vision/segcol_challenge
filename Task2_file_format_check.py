9e90d282bee31ed8a7b026a78e82ebaa247de8c3
XinweiJu <xinwei.ju.22@ucl.ac.uk>
2024-05-15 19:03:53 +0100

    add Task2 file format check and length check

--- Task2_file_format_check.py
+++ Task2_file_format_check.py
@@ -1,16 +1,48 @@
 import re
 
-# Check if the file format is correct
-def check_file_format(file):
+def is_file_format_correct(file):
+    """Check if the file format is correct."""
     with open(file, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            if not re.match(r'Seq_\d{3}_\d+/imgs/.*\.png', line.strip()): #/frame_\d+\.png', line.strip()):
+            if not re.match(r'Seq_\d{3}_\d+/imgs/.*\.png', line.strip()):
                 return False
     return True
 
-check_result = check_file_format('data/output/samples_xtest.txt')
-if check_result:
-    print('File format is correct')
-else:
-    print('File format is incorrect')
+def is_sample_list_length_valid(file):
+    """Check if the sample list length is no more than 400."""
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        if len(lines) > 400:
+            return False
+    return True
+
+
+# Check whether the next line in sample content is repeated
+def check_repeated_samples(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        if len(lines) != len(set(lines)):
+            return False
+    return True
+
+
+def main():
+    """Main function to run the checks."""
+    file_path = 'data/output/samples_xtest.txt'
+    
+    if is_file_format_correct(file_path):
+        print('Congrats! File format is correct.')
+    else:
+        print('File format is incorrect, please check!')
+
+    if is_sample_list_length_valid(file_path):
+        print('Congrats! Sample list length is less than 400.')
+    else:  
+        print('Warning: Sample list length is more than 400, only the first 400 samples will be used for evaluation')
+
+    if not check_repeated_samples(file_path):
+        print('Your sample list contains repeated samples, please check!')
+
+if __name__ == "__main__":
+    main()
