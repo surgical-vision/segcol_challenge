fixed average precision and merging

eval.py
@@ -1,96 +1,58 @@
 import numpy as np
 from PIL import Image
-from metrics import *
 import glob
+import json
+import os
+import torch
+from metrics import find_optimal_thresholds, check_for_zeros, OIS, ODS, compute_dice, AP, compute_CLDice
 
+def load_image(file_path, npy=False):
+    try:
+        if npy:
+            return np.load(file_path)
+        else:
+            img = Image.open(file_path)
+            img_data = np.array(img)
+            class_channels = [np.where(img_data == val, 1, 0) for val in [255, 127, 128, 129]]
+            return np.stack(class_channels, axis=-1)
+    except Exception as e:
+        print(f"Error loading image {file_path}: {e}")
+        return None
 
-def get_images(file, npy=False):
-    if npy:
-        img_data = np.load(file)
+def evaluate_classes(pred_list, gt_list, num_classes):
+    # calculate the dice score, optimal thresholds, AP, CLDice, ODS, OIS
+    if num_classes == 2:
+        pred_list = [np.stack([img[:,:,0], np.max(img[:,:,1:4], axis=2)], axis=2) for img in pred_list]
+        gt_list = [np.stack([img[:,:,0], np.max(img[:,:,1:4], axis=2)], axis=2) for img in gt_list]
+    
+    optimal_thresholds_orig = find_optimal_thresholds(pred_list, gt_list, num_classes=num_classes)
+    pred_list_handled, gt_list_handled = check_for_zeros(pred_list, gt_list, optimal_thresholds_orig, num_classes=num_classes)
+    ois = OIS(pred_list_handled, gt_list_handled, np.linspace(0.01, 0.99, 99), num_classes=num_classes)
+    ods, optimal_thresholds_ods = ODS(pred_list_handled, gt_list_handled, np.linspace(0.01, 0.99, 99), num_classes=num_classes)
+    optimal_thresholds = [torch.tensor(npy_array[0], dtype=torch.float64) for npy_array in optimal_thresholds_ods]
+    dice_score = compute_dice(pred_list_handled, gt_list_handled, optimal_thresholds)
+    ap = AP(pred_list_handled, gt_list_handled, thresholds=list(np.array(optimal_thresholds)), num_classes=num_classes, average=None)
+    clDice_score = compute_CLDice(pred_list_handled, gt_list_handled, optimal_thresholds, num_classes=num_classes)
+    
+    print(f"{num_classes} class evaluation:")
+    print("ods", ods)
+    print("ois", ois)
+    print("dice_score", np.array(dice_score))
+    print("optimal_thresholds", np.array(optimal_thresholds))
+    print("ap", np.array(ap))
+    print("clDice_score", np.array(clDice_score))
 
+def main():
+    folder = "data/output/Seq*/predictions/" # change according to final structure
 
-    if not npy:
-        # Open the image
-        img = Image.open(file)
+    # get pred_list and gt_list by reading images from the directory
+    # the prediction data should be multichannel/class (4) tif files.
+    files = sorted(glob.glob(folder + "*.npy"))
+    pred_list = [load_image(file, npy = True) for file in files]
+    gt_list = [load_image(file.replace("output", "input").replace("predictions", "segm_maps").replace(".npy", ".png")) for file in files]
+    
+    evaluate_classes(pred_list, gt_list, 2)
+    evaluate_classes(pred_list, gt_list, 4)
 
-        # Convert the image data to a numpy array
-        img_data = np.array(img)
-
-        # Create class channels using np.where
-        class1 = np.where(img_data == 255, 1, 0) # fold
-        class2 = np.where(img_data == 127, 1, 0) # tool1 
-        class3 = np.where(img_data == 128, 1, 0) # tool2
-        class4 = np.where(img_data == 129, 1, 0) # tool3
-
-        # Stack all class channels together
-        img_data = np.stack((class1, class2, class3, class4), axis=-1)
-
-    return img_data
-
-# initialize lists and variables                         
-pred_list = []
-gt_list = []
-pred_list_orig = []
-thresh_list = np.linspace(0.01, 0.99, 99)
-folder = "data/output/Seq*/predictions/" # change according to final structure
-
-# if we want the results per seg add another for loop
-
-
-# get pred_list and gt_list by reading images from the directory
-# the prediction data should be multichannel/class (4) tif files.
-for file in sorted(glob.glob(folder + "*.npy")):
-    pred_list.append(get_images(file, npy = True))
-    gt_list.append(get_images(file.replace("output", "input").replace("predictions", "segm_maps").replace(".npy", ".png")))
-
-
-#----------------------------------------------------------------------------------------------------
-print('Four class evaluation:')
-
-num_classes = 4
-
-# calculate the dice score, optimal thresholds, AP, CLDice, ODS, OIS
-optimal_thresholds_orig = find_optimal_thresholds(pred_list, gt_list, num_classes = num_classes) #[torch.tensor(0.75, dtype=torch.float64), torch.tensor(0.75, dtype=torch.float64)]
-pred_list_handled, gt_list_handled = check_for_zeros(pred_list, gt_list, optimal_thresholds_orig, num_classes = num_classes)
-
-ois = OIS(pred_list_handled, gt_list_handled, thresh_list, num_classes=num_classes)
-ods, optimal_thresholds_ods = ODS(pred_list_handled, gt_list_handled, thresh_list, num_classes=num_classes)
-print("ods", ods)
-print("ois", ois)
-
-optimal_thresholds = [torch.tensor(npy_array[0], dtype=torch.float64) for npy_array in optimal_thresholds_ods]
-dice_score = compute_dice(pred_list_handled, gt_list_handled, optimal_thresholds)
-ap = AP(pred_list_handled, gt_list_handled, thresholds = list(np.array(optimal_thresholds)), num_classes = num_classes, average = None)
-clDice_score = compute_CLDice(pred_list_handled, gt_list_handled, optimal_thresholds, num_classes = num_classes)
-print("dice_score", np.array(dice_score))
-print("optimal_thresholds", np.array(optimal_thresholds))
-print("ap", np.array(ap))
-print("clDice_score", np.array(clDice_score))
-
-
-
-#----------------------------------------------------------------------------------------------------
-print('Two class evaluation:')
-# tool considered as 1 class
-pred_list_2class = [np.stack([img[:,:,0], np.max(img[:,:,1:4], axis=2)], axis=2) for img in pred_list]
-gt_list_2class = [np.stack([img[:,:,0], np.max(img[:,:,1:4], axis=2)], axis=2) for img in gt_list]
-num_classes = 2
-
-# calculate the dice score, optimal thresholds, AP, CLDice, ODS, OIS
-optimal_thresholds_orig_2class = find_optimal_thresholds(pred_list_2class, gt_list_2class, num_classes = num_classes)
-pred_list_handled_2class, gt_list_handled_2class = check_for_zeros(pred_list_2class, gt_list_2class, optimal_thresholds_orig_2class, num_classes = num_classes)
-
-ois_2class = OIS(pred_list_handled_2class, gt_list_handled_2class, thresh_list, num_classes=num_classes)
-ods_2class, optimal_thresholds_ods_2class = ODS(pred_list_handled_2class, gt_list_handled_2class, thresh_list, num_classes=num_classes)
-print("ods", ods_2class)
-print("ois", ois_2class)
-
-optimal_thresholds_2class = [torch.tensor(npy_array[0], dtype=torch.float64) for npy_array in optimal_thresholds_ods_2class]
-dice_score_2class = compute_dice(pred_list_handled_2class, gt_list_handled_2class, optimal_thresholds_2class)
-ap_2class = AP(pred_list_handled_2class, gt_list_handled_2class, thresholds = list(np.array(optimal_thresholds_2class)), num_classes = num_classes, average = None)
-clDice_score_2class = compute_CLDice(pred_list_handled_2class, gt_list_handled_2class, optimal_thresholds_2class, num_classes = num_classes)
-print("dice_score", np.array(dice_score_2class))
-print("optimal_thresholds", np.array(optimal_thresholds_2class))
-print("ap", np.array(ap_2class))
-print("clDice_score", np.array(clDice_score_2class))
-
+if __name__ == "__main__":
+    main()

merge.py
@@ -6,6 +6,8 @@ import os
 import time, platform
 import numpy as np
 import cv2
+import glob
+import tifffile as tiff
 
 
 def numpy_load(file_path1, file_path2):
@@ -14,54 +16,39 @@ def numpy_load(file_path1, file_path2):
     return array1, array2
     
 
-def parse_args():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description='DexiNed merge')
-    
-    # Data parameters
-    parser.add_argument('--folder1',
-                        type=str,
-                        default=None,
-                        help='the path to the directory with the output from folds, 1 channel.')
-    parser.add_argument('--folder2',
-                        type=str,
-                        default=None,
-                        help='the path to the directory with the output from tool, 4 channels.')
-    parser.add_argument('--output_merge_dir',
-                        type=str,
-                        default='./merged_results',
-                        help='the path to output the results.')
-    
-    args = parser.parse_args()
-    return args
-
-
-def main(args):
+def main(folder1, folder2, output_merge_dir):
     """Main function."""
 
     print('Merge two numpy')
 
     # Define folder path
-    merged_folder_path = args.output_merge_dir
+    merged_folder_path = output_merge_dir
 
     if not os.path.exists(merged_folder_path):
         os.makedirs(merged_folder_path)
-    merged_file_path = './merged_results/merged_array.npy'
-    # merge
-    array1 = np.random.rand(1, 480, 640)
-    array2 = np.random.rand(4, 480, 640)
-    # Change the first channel of the loaded second array to be the loaded first array
-    array2[0, :, :] = array1[0, :, :]
-    # Save 
-    np.save(merged_file_path, array2)
-    # print('shape', array2.shape)
+    for file in glob.glob(os.path.join(folder1,'*','imgs','*.npy')):
+        print('file', file)
+        file_path1 = file
+        file_path2 = file.replace(folder1, folder2)
+        print('file_path2', file_path2)
+        array1, array2 = numpy_load(file_path1, file_path2)
+        # Change the first channel of the loaded second array to be the loaded first array
+        merged_array = array2.copy()
+        merged_array[0, :, :] = array1
+        # Save
+        merged_file_path = file.replace(folder1, merged_folder_path)
+        os.makedirs(os.path.dirname(merged_file_path), exist_ok=True)
+        np.save(merged_file_path, merged_array.transpose(1, 2, 0))
+        print('shape', merged_array.shape)
 
     
     print('-------------------------------------------------------')
     print('Finish merge')
     print('-------------------------------------------------------')
 
-if __name__ == '__main__':
-    args = parse_args()
-    main(args)
+# merge two folders of numpy files such that the first channel of the second folder is replaced by the first folder
+folder1 = "data/output/Seq*/predictions1/"
+folder2 = "data/output/Seq*/predictions2/"
+output_merge_dir = "data/output/Seq*/predictions/"
+main(folder1, folder2, output_merge_dir)
 

metrics.py
@@ -1,7 +1,7 @@
 # metrics: Dice, AP, OIS, ODS, CLDice
 # for ranking: mAP
 
-from torchmetrics.functional.classification import multilabel_average_precision, dice
+from torchmetrics.functional.classification import binary_average_precision, dice
 from torchmetrics.classification import MultilabelROC
 import numpy as np
 
@@ -51,7 +51,6 @@ def find_optimal_thresholds(pred_list, gt_list, num_classes):
     metric = MultilabelROC(num_labels=num_classes, thresholds=None)
     fpr, tpr, thresholds = metric(pred_list_tensor, gt_list_tensor)
     optimal_thresholds = []
-    dice_list = []
     for i in range(len(tpr)):
         J = tpr[i] - fpr[i]  # Youden's index
         idx = np.argmax(J)
@@ -81,15 +80,15 @@ def AP(pred_list, gt_list, thresholds, num_classes = 4, average = None):
     transform = transforms.Compose([
         transforms.ToTensor(),  # Converts image to tensor with values in [0, 1]
     ])
-    gt_list_tensor = [None] * len(pred_list)
-    pred_list_tensor = gt_list_tensor.copy()
-    for i in range(len(pred_list)):
-        gt_list_tensor[i] = transform(gt_list[i])
-        pred_list_tensor[i] = transform(pred_list[i])
+    gt_list_tensor = [transform(gt) for gt in gt_list]
+    pred_list_tensor = [transform(pred) for pred in pred_list]
+
     # input shape: [n, num_classes, w, h]
     gt_list_tensor = torch.stack(gt_list_tensor, dim=0)
     pred_list_tensor = torch.stack(pred_list_tensor, dim=0)
-    AP = multilabel_average_precision(pred_list_tensor, gt_list_tensor, num_classes, average, thresholds=thresholds)#None)
+    AP = []
+    for i in range(num_classes):
+        AP.append(binary_average_precision(pred_list_tensor[:,i,:,:], gt_list_tensor[:,i,:,:], thresholds=[thresholds[i]])) #None)
     return AP
 
 
@@ -194,7 +193,6 @@ def compute_CLDice(pred_list, gt_list, optimal_thresholds, num_classes=4):
             total_clDice += score
         average_clDice_perclass.append(total_clDice / float(len(pred_list)))
     return average_clDice_perclass
-
 
 
 